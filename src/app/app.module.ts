3bba1bd86f06167a7e644ee9f8ee6593a89fce2f
giovanny0930 <giovanny.carbonell@gmail.com>
2017-09-09 17:30:00 -0500

    creando links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import{ RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+import {CustomersModule} from './customers/customers.module';
+import {MenuComponent} from './menu/menu.component';
+import {OrdersModule } from './orders/orders.module';
+import{IndexComponent}from './customers/index/index.component';
+import {AddComponent as OrderAddComponent}from './orders/add/add.component';
+
+const appRoutes:Routes =[
+
+  {path: "customers/index", component:IndexComponent},
+  {path: "orders/add", component:IndexComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomersModule,
+    ordersmodule,
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
