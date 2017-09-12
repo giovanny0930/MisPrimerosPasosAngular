9a18a9b25cd1b5e031b4171f4ee85c57e8251e6c
giovanny0930 <giovanny.carbonell@gmail.com>
2017-09-11 21:59:24 -0500

    creando menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,42 @@ import {OrdersModule } from './orders/orders.module';
 import{IndexComponent}from './customers/index/index.component';
 import {AddComponent as OrderAddComponent}from './orders/add/add.component';
 import { CurrentOptionComponent } from "./current-option/current-option.component";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {
+  
+  MdAutocompleteModule,  
+  MdButtonModule,  
+  MdButtonToggleModule,  
+  MdCardModule,  
+  MdCheckboxModule,   
+  MdChipsModule,  
+  MdCoreModule,   
+  MdDatepickerModule,   
+  MdDialogModule,  
+  MdExpansionModule,  
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,  
+  MdListModule,
+  MdMenuModule,  
+  MdNativeDateModule,
+  MdPaginatorModule,    
+  MdProgressBarModule,   
+  MdProgressSpinnerModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,  
+  } from
+  '@angular/material';
 
 const appRoutes:Routes =[
 
@@ -23,13 +59,46 @@ const appRoutes:Routes =[
   declarations: [
     AppComponent,
     MenuComponent,
-    CurrentOptionComponent
+    CurrentOptionComponent,
+       
   ],
   imports: [
     BrowserModule,
     CustomersModule,
     OrdersModule,
-    RouterModule.forRoot(appRoutes) 
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MdAutocompleteModule,  
+    MdButtonModule,  
+    MdButtonToggleModule,  
+    MdCardModule,  
+    MdCheckboxModule,   
+    MdChipsModule,  
+    MdCoreModule,   
+    MdDatepickerModule,   
+    MdDialogModule,  
+    MdExpansionModule,  
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,  
+    MdListModule,
+    MdMenuModule,  
+    MdNativeDateModule,
+    MdPaginatorModule,    
+    MdProgressBarModule,   
+    MdProgressSpinnerModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
